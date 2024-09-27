09a89e9208ac00681059c60939ab39f4685a3909
mitso-ksr <mitsonary@gmail.com>
2024-09-27 20:33:11 +0330

    style event page

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,8 @@
-import Link from "next/link";
 import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem(props) {
   const { title, image, date, location, id } = props.event;
@@ -18,14 +21,21 @@ export default function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humenReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddres}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
